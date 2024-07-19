1d76219339f55abc182075675ecd31b98c0a4b09
Matteo DH <matthdserver@gmail.com>
2024-07-19 13:03:57 +0700

    none-ls

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,5 @@
+vim.g.mapleader= " "
+vim.keymap.set('n', ';', ':', { noremap = true, silent = false })
+vim.keymap.set('v',  ';', ':', { noremap = true, silent = false })
+vim.keymap.set('n', 'e', ':Explore<CR>', { noremap = true, silent = true })
+
---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "rust_analyzer" },
+        automatic_installation = true,
+      })
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.rust_analyzer.setup({
+        settings = {
+          ["rust-analyzer"] = {},
+        },
+      })
+      lspconfig.lua_ls.setup({})
+
+      local opts = { noremap = true, silent = true }
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+      vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+    end,
+  },
+}
